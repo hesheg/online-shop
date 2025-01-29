831da7513778fc95d1c1a3c9f168f6c4b09be11f
heshegte <heshiktsy@gmail.com>
2025-01-29 20:20:38 +0500

    Добавляет пхпфайл который обрабатывает форму регистрации

---
+++ src/public/handle_registration_form.php
@@ -0,0 +1,59 @@
+<?php
+
+echo "<pre>";
+//print_r($_GET);
+
+$name = $_GET['name'];
+$email = $_GET['email'];
+$password = $_GET['psw'];
+$passwordRep = $_GET['psw-repeat'];
+
+
+if (ctype_alpha($name)) {
+    echo '';
+} else {
+    exit('В имени не должны быть цифры и другие знаки. Только латинские буквы');
+}
+
+$nameValid = '';
+if (strlen($name) >= 2 && strlen($name) <= 50) {
+    $nameValid .= $name;
+} else {
+    exit('Имя не должно иметь меньше 2 или больше 50 символов');
+}
+
+$emailToLower = strtolower($email);
+$emailValid = trim($emailToLower);
+
+
+if ($password === $passwordRep) {
+    echo '';
+} else {
+    exit('Неправильно введен повторный пароль');
+}
+
+
+$passwordValid = '';
+
+if (strlen($password) >= 8 && strlen($password) <= 20) {
+    $passwordValid .= $password;
+} else {
+    exit('Длина пароля должна быть больше или равно 8 символам и меньше или равно 20 символам');
+}
+
+
+$pdo = new PDO("pgsql:host=db; port=5432; dbname=db;", username: "dbuser", password: "dbpwd");
+
+$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$nameValid', '$emailValid', '$passwordValid')");
+echo "\n";
+
+
+$statement = $pdo->query("SELECT count(*) FROM users");
+$data = $statement->fetch();
+echo "<pre>";
+//print_r($data);
+
+
+$statement = $pdo->query("SELECT * FROM users WHERE id = 20");
+$res = $statement->fetch();
+//print_r($res);
--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo 'Hello, WWWorld!!!';
+$pdo = new PDO("pgsql:host=db; port=5432; dbname=db;", username: "dbuser", password: "dbpwd");
+
+//$pdo->exec("INSERT INTO users (name, email, password) VALUES ('Dasha', 'dasha@mail.ru', '4321')");
+
+$statement = $pdo->query("SELECT * FROM users");
+$data = $statement->fetch();
+echo "<pre>";
+print_r($data);
+
